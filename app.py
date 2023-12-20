134469719e85e1261a8818187e1d25643f453885
luis naranjo <47650944+Luisill0@users.noreply.github.com>
2023-12-20 23:01:58 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,110 @@
+from database import Database, Movie, User
+from flask import Flask, request, render_template, redirect, session
+
+from icecream import ic
+
+Database.verifyTables(2)
+
+app = Flask("Flask - Lab")
+app.secret_key = "secret"
+
+@app.route("/", methods=["GET"])
+def index():
+    username = session.get("username")
+    admin = session.get("admin") if session.get("admin") is not None else False
+    logged = username is not None
+    if username is None: username = "guest"
+    database = Database()
+    movies = database.getMovies()
+    full_render = render_template("navbar.html", username=username, admin=admin, logged=logged)
+    full_render += render_template("home.html", username=username, logged=logged, admin=admin, movies=movies)
+    if admin: full_render += render_template("movie-form.html")
+    return full_render
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET": return render_template("login.html")
+    if request.method != "POST": return redirect("/")
+    database = Database()
+    
+    username = request.form.get("username")
+    password = request.form.get("password")
+    maybeUser = database.getUser(username, password)
+    if maybeUser is None: return render_template("login.html", error=True)
+
+    session["username"] = maybeUser.username
+    session["admin"] = maybeUser.admin
+    session.permanent = True
+    return redirect("/")
+
+@app.route("/add-movie", methods=["POST"])
+def addMovie():
+    movie: Movie = Movie(
+        id=None,
+        name=request.form.get("name"),
+        year=request.form.get("year"),
+        director=request.form.get("director"),
+        duration=request.form.get("duration"),
+        poster=request.form.get("poster")
+    )
+    database = Database()
+    database.addMovie(movie)
+    return redirect("/")    
+
+@app.route("/add-user", methods=["POST"])
+def addUser():
+    user: User = User(
+        id=None,
+        username=request.form.get("username"),
+        password=request.form.get("password"),
+        admin=request.form.get("admin") is not None
+    )
+    database = Database()
+    database.createUser(user)
+    return redirect("/users")
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.pop("username")
+    session.pop("admin")
+    return redirect("/")
+
+@app.route("/users", methods=["GET"])
+def users():
+    if session.get("username") is None or session.get("admin") is False: return redirect("/")
+    database = Database()
+    users = database.getUsers()
+    return render_template("navbar.html", username=session.get("username"), admin=session.get("admin"), logged=True) + render_template("users.html", users=users)
+
+def getMovieTable(movies: list[tuple]) -> str:
+    table = """
+        <div class="table-container">
+        <h1>Movie List</h1>
+        <table>
+        <tr>
+            <th>Name</th>
+            <th>Year</th>
+            <th>Director</th>
+            <th>Duration</th>
+            <th>Poster</th>
+        </tr>
+    """
+    for movie in movies:
+        table += f"""
+            <tr>
+                <td>{movie[1]}</td>
+                <td>{movie[2]}</td>
+                <td>{movie[3]}</td>
+                <td>{movie[4]} min.</td>
+                <td>
+                <a target="_blank" rel="norreferer" href="{movie[5]}">
+                    <img src={movie[5]} height=75 /> 
+                </a>
+                </td>
+            </tr>
+        """
+    table += "</table></div>"
+    return table
+
+app.debug = True
+app.run()
---
+++ database.py
@@ -0,0 +1,78 @@
+import sqlite3
+
+from contextlib import closing
+from icecream import ic
+from pydantic import BaseModel
+
+from typing import Optional
+
+class User(BaseModel):
+    id: Optional[int]
+    username: str
+    password: str
+    admin: bool
+
+class Movie(BaseModel):
+    id: Optional[int]
+    name: str
+    year: int
+    director: str
+    duration: int
+    poster: str
+
+DATABASE_NAME = "movie-app.db"
+
+class Database:
+    @staticmethod
+    def verifyTables(verbose=2):
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+            if verbose > 0: print("Connected to SQLite!")
+            with closing(connection.cursor()) as cursor:
+                cursor.execute("CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY, name TEXT, year INTEGER, director TEXT, duration INTEGER, poster TEXT);")
+                cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, admin INTEGER);")
+                cursor.execute('INSERT OR IGNORE INTO users VALUES(1, "luis", "secret", 1);')    
+                tables = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+                if verbose > 0: print("Tables loaded: ", end="")
+                if verbose > 1: print(tables)
+            connection.commit()      
+    
+    def addMovie(self, movie: Movie):
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                values = f'NULL, "{movie.name}", "{movie.year}", "{movie.director}", "{movie.duration}", "{movie.poster}"'
+                cursor.execute(f"INSERT INTO movies VALUES({values})")
+            connection.commit()    
+
+    def getMovies(self) -> list[Movie]:
+        movie_list = []
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                movies_tuples = cursor.execute("SELECT * FROM movies").fetchall()
+                for movie_tuple in movies_tuples:
+                    movie_list.append(Movie(id=movie_tuple[0], name=movie_tuple[1], year=movie_tuple[2], director=movie_tuple[3], duration=movie_tuple[4], poster=movie_tuple[5]))
+        return movie_list    
+
+    def createUser(self, user:User):
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+                with closing(connection.cursor()) as cursor:
+                    values = f'NULL, "{user.username}", "{user.password}", {1 if user.admin is True else 0}'
+                    cursor.execute(f"INSERT INTO users VALUES({values})")
+                connection.commit()
+
+    def getUser(self, username: str, password:str) -> User | None:
+        user = None
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                maybeUser = cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password,)).fetchone()
+                if maybeUser is not None:
+                    user = User(id=maybeUser[0], username=maybeUser[1], password=maybeUser[2], admin=maybeUser[3] > 0)
+        return user
+    
+    def getUsers(self) -> list[User]:
+        users = []
+        with closing(sqlite3.connect(DATABASE_NAME)) as connection:
+            with closing(connection.cursor()) as cursor:
+                usersTuples = cursor.execute("SELECT * FROM users").fetchall()
+                for userTuple in usersTuples:
+                    users.append(User(id=userTuple[0], username=userTuple[1], password=userTuple[2], admin=userTuple[3] > 0))
+        return users
